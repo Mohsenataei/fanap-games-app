58eb38879fb947de1427cb8585f24a4d8e562ac4
Alireza_Lavasani <alireza.sh.lavasani@gmail.com>
2021-06-01 09:50:11 +0430

    buildSrc module added.

---
+++ buildSrc/src/main/kotlin/ataei/libs/Android.kt
@@ -0,0 +1,11 @@
+package ataei.libs
+
+object Android {
+
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 30
+    const val compileSdkVersion = 30
+    const val applicationId = "com.example.ataei"
+    const val versionCode = 1
+    const val versionName = "0.1"
+}
